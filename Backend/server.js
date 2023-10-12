ce2be8bb1e52df284c4add23fcbde33a2739083c
Malmi Hewakapuge <97165436+malmi20@users.noreply.github.com>
2023-10-12 12:31:15 +0530

    server.js change

--- Backend/server.js
+++ Backend/server.js
@@ -4,3 +4,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//DB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+     //listening for request
+     app.listen(process.env.PORT, ()=> {
+     console.log("DB connected and listening on port ", process.env.PORT)
+     })
+})
+.catch((error)=>{
+    console.log(error)
+})
