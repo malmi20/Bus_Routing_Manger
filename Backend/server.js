cd6b65f3307709a3e3ffdaf1d23d21781a8dbe61
Malmi Hewakapuge <97165436+malmi20@users.noreply.github.com>
2023-10-13 21:34:09 +0530

    server edited

--- Backend/server.js
+++ Backend/server.js
@@ -1,53 +1,66 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 
-//express app
-const app = express();
+class Server {
+  static instance;
 
-//middleware
-app.use(express.json()); //to add json to the 'req' Object
+  constructor() {
+    if (Server.instance) {
+      return Server.instance;
+    }
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+    this.app = express();
 
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-app.use(bodyParser.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//routes
-app.use(`/api/user`, require("./routes/user/userRoutes"));
-app.use(`/api/buses`, require("./routes/bus/busRoutes"));
-app.use(`/api/busDetails`, require("./routes/bus/busDetailsRoutes"));
-
-app.use((err, _req, res, _next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something Went Wrong, Please Contact Technical Team.";
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
-});
-
-//connect to DB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    // listen to port
-    app.listen(process.env.PORT, () => {
-      console.log(
-        "Connected to db & listening for requests on port",
-        process.env.PORT
-      );
+    // Middleware
+    this.app.use(express.json());
+    this.app.use((req, res, next) => {
+      console.log(req.path, req.method);
+      next();
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+
+    const bodyParser = require('body-parser');
+    const cors = require('cors');
+    this.app.use(bodyParser.json());
+    this.app.use(cors());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+
+    // Routes
+    this.app.use(`/api/user`, require('./routes/user/userRoutes'));
+    this.app.use(`/api/buses`, require('./routes/bus/busRoutes'));
+    this.app.use(`/api/busDetails`, require('./routes/bus/busDetailsRoutes'));
+
+    this.app.use((err, _req, res, _next) => {
+      const errorStatus = err.status || 500;
+      const errorMessage =
+        err.message || 'Something Went Wrong, Please Contact Technical Team.';
+      return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+      });
+    });
+
+    // Connect to DB
+    mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => {
+        // Listen to port
+        this.app.listen(process.env.PORT, () => {
+          console.log(
+            'Connected to db & listening for requests on port',
+            process.env.PORT
+          );
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    Server.instance = this;
+  }
+}
+
+const serverInstance = new Server();
+module.exports = serverInstance.app;
